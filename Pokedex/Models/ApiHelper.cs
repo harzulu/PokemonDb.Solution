a2268c22848eaef259fa5fb79c550b0d7b793980
Jeff Chiu <jwchiu82@gmail.com>
2021-01-20 14:54:24 -0800

    add post put and delete functionality
    Co-authored-by: Ben McFarland <benrmcfarland@gmail.com>

--- Pokedex/Models/ApiHelper.cs
+++ Pokedex/Models/ApiHelper.cs
@@ -21,6 +21,30 @@ namespace Pokedex.Models
             return response.Content;
         }
 
-        //public static async Task Post(string newPokemon)
+        public static async Task Post(string newPokemon)
+        {
+            RestClient client = new RestClient("http://localhost:5000/api");
+            RestRequest request = new RestRequest($"pokemon", Method.POST);
+            request.AddHeader("Content-Type", "application/json");
+            request.AddJsonBody(newPokemon);
+            var response = await client.ExecuteTaskAsync(request);
+        }
+
+        public static async Task Put(int id, string newPokemon)
+        {
+            RestClient client = new RestClient("http://localhost:5000/api");
+            RestRequest request = new RestRequest($"pokemons/{id}", Method.PUT);
+            request.AddHeader("Content-Type", "application/json");
+            request.AddJsonBody(newPokemon);
+            var response = await client.ExecuteTaskAsync(request);
+        }
+
+        public static async Task Delete(int id)
+        {
+            RestClient client = new RestClient("http://localhost:5000/api");
+            RestRequest request = new RestRequest($"pokemons/{id}", Method.DELETE);
+            request.AddHeader("Content-Type", "application/json");
+            var response = await client.ExecuteTaskAsync(request);
+        }
     }
 }

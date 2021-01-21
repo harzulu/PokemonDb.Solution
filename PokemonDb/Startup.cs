2344453d9a91b4f4688f2b532d84851505093d2b
Jeff Chiu <jwchiu82@gmail.com>
2021-01-21 10:32:55 -0800

    add SwaggerUI
    Co-authored-by: Ben McFarland <benrmcfarland@gmail.com>

--- PokemonDb/Startup.cs
+++ PokemonDb/Startup.cs
@@ -20,24 +20,42 @@ namespace PokemonDb
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<PokemonDbContext> (opt => opt.UseMySql(Configuration.GetConnectionString("Defaultconnection")));
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            // services.AddDbContext<PokemonDbContext> (opt => opt.UseMySql(Configuration.GetConnectionString("Defaultconnection")));
+            // services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddDbContext<PokemonDbContext> (opt => opt.UseInMemoryDatabase("PokemonDb"));
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSwaggerGen();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+        //This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        // {
+        //     if (env.IsDevelopment())
+        //     {
+        //         app.UseDeveloperExceptionPage();
+        //     }
+        //     else
+        //     {
+        //         // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        //         app.UseHsts();
+        //     }
 
-           //app.UseHttpsRedirection();
+        //    //app.UseHttpsRedirection();
+        //     app.UseMvc();
+        // }
+        public void Configure(IApplicationBuilder app)
+        {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = string.Empty;
+            });
+
             app.UseMvc();
         }
     }
